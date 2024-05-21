7de6c94291561cf0a532e2710e4376819e981655
asivers <sivers295@gmail.com>
2024-05-21 13:29:05 +0200

    template - dynamically create year calendar

--- app/src/main/java/com/example/mycalendar/YearActivity.kt
+++ app/src/main/java/com/example/mycalendar/YearActivity.kt
@@ -2,30 +2,50 @@ package com.example.mycalendar
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 // just a template yet
 @SuppressLint("NewApi")
 class YearActivity : ComponentActivity() {
 
-    private lateinit var backToMonthButton: Button
+    private lateinit var yearCalendarLayout: GridLayout
+    private lateinit var monthsWithNames: Array<LinearLayout>
+    private lateinit var dayCells: Array<Array<TextView>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_year)
 
-        backToMonthButton = findViewById(R.id.back_to_month_btn)
-
-        backToMonthButton.setOnClickListener {
-            val intent = Intent(this@YearActivity, MonthActivity::class.java)
-            startActivity(intent)
-
-            // not sure if it helps
-            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0, Color.TRANSPARENT)
+        yearCalendarLayout = findViewById(R.id.year_calendar_layout)
+        monthsWithNames = Array(12) { layoutInflater.inflate(R.layout.month_year_cell, yearCalendarLayout, false) as LinearLayout }
+        monthsWithNames.forEach {
+            yearCalendarLayout.addView(it)
+            it.setOnClickListener { goToMonthPage() }
         }
+        dayCells = Array(12) {
+            val monthCell = monthsWithNames[it].getChildAt(1) as GridLayout
+            Array(42) { layoutInflater.inflate(R.layout.day_year_cell, monthCell, false) as TextView }
+        }
+        dayCells.forEachIndexed { index, dayCellsForMonth ->
+            val monthCell = monthsWithNames[index].getChildAt(1) as GridLayout
+            dayCellsForMonth.forEach {
+                monthCell.addView(it)
+                it.setOnClickListener { goToMonthPage() }
+            }
+        }
+    }
+
+    private fun goToMonthPage() {
+        val intent = Intent(this@YearActivity, MonthActivity::class.java)
+        startActivity(intent)
+
+        // todo consider this
+//        overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0, Color.TRANSPARENT)
+        // and save month activity state (bundle)
     }
 }
