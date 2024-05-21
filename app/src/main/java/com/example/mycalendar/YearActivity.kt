97d33011a64f6d133a00db008bb7a4de8397d4d0
asivers <sivers295@gmail.com>
2024-05-21 13:32:43 +0200

    template - year view layout (won't be used, just helps to develop layout)

--- app/src/main/java/com/example/mycalendar/YearActivity.kt
+++ app/src/main/java/com/example/mycalendar/YearActivity.kt
@@ -11,21 +11,21 @@ import androidx.activity.ComponentActivity
 @SuppressLint("NewApi")
 class YearActivity : ComponentActivity() {
 
-    private lateinit var backToMonthButton: Button
+//    private lateinit var monthViewButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_year)
 
-        backToMonthButton = findViewById(R.id.back_to_month_btn)
-
-        backToMonthButton.setOnClickListener {
-            val intent = Intent(this@YearActivity, MonthActivity::class.java)
-            startActivity(intent)
-
-            // not sure if it helps
-            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0, Color.TRANSPARENT)
-        }
+//        monthViewButton = findViewById(R.id.month_view_btn)
+//
+//        monthViewButton.setOnClickListener {
+//            val intent = Intent(this@YearActivity, MonthActivity::class.java)
+//            startActivity(intent)
+//
+//            // not sure if it helps
+//            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0, Color.TRANSPARENT)
+//        }
     }
 }
