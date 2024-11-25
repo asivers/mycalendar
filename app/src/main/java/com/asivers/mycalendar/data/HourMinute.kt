b1d8e422e72925b76485e47f45560909f5302f66
asivers <sivers295@gmail.com>
2024-11-25 01:52:56 +0100

    made NoteInfo comparable

--- app/src/main/java/com/asivers/mycalendar/data/HourMinute.kt
+++ app/src/main/java/com/asivers/mycalendar/data/HourMinute.kt
@@ -6,4 +6,14 @@ import kotlinx.serialization.Serializable
 data class HourMinute(
     val hour: Int,
     val minute: Int
-)
+): Comparable<HourMinute> {
+    override fun compareTo(other: HourMinute) = compareValuesBy(this, other,
+        { it.hour },
+        { it.minute }
+    )
+    override fun toString(): String {
+        val fullHour = hour.toString().padStart(2, '0')
+        val fullMinute = minute.toString().padStart(2, '0')
+        return "$fullHour:$fullMinute"
+    }
+}
--- app/src/main/java/com/asivers/mycalendar/data/NoteInfo.kt
+++ app/src/main/java/com/asivers/mycalendar/data/NoteInfo.kt
@@ -5,5 +5,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class NoteInfo(
     val msg: String,
-    val time: HourMinute? = null
-)
+    val time: HourMinute? = null,
+    val order: Int
+): Comparable<NoteInfo> {
+    override fun compareTo(other: NoteInfo) = compareValuesBy(this, other,
+        { it.time },
+        { it.order }
+    )
+}
