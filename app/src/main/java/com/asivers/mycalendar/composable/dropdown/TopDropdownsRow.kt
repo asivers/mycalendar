ddfc50a23eb5e1db0c5737c5091d948ef449798a
asivers <sivers295@gmail.com>
2025-04-01 23:12:33 +0200

    added changing year icons, temporary empty

--- app/src/main/java/com/asivers/mycalendar/composable/dropdown/TopDropdownsRow.kt
+++ app/src/main/java/com/asivers/mycalendar/composable/dropdown/TopDropdownsRow.kt
@@ -1,18 +1,21 @@
 package com.asivers.mycalendar.composable.dropdown
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.asivers.mycalendar.R
 import com.asivers.mycalendar.data.SchemeContainer
 import com.asivers.mycalendar.data.SelectedDateInfo
+import com.asivers.mycalendar.utils.getYearIconId
 
 @Composable
 fun TopDropdownsRow(
@@ -29,11 +32,15 @@ fun TopDropdownsRow(
             .padding(18.dp, 0.dp, 16.dp, 0.dp),
     ) {
         if (forYearView) {
-            Image(
-                modifier = Modifier.padding(4.dp, 0.dp),
-                painter = painterResource(id = R.drawable.year_rat),
-                contentDescription = "Symbol of the year icon"
-            )
+            Box(
+                modifier = Modifier.width(93.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = getYearIconId(selectedDateInfo.year)),
+                    contentDescription = "Symbol of the year icon"
+                )
+            }
         } else {
             SelectMonthDropdown(
                 modifier = Modifier.wrapContentWidth(),
--- app/src/main/java/com/asivers/mycalendar/utils/AssetUtils.kt
+++ app/src/main/java/com/asivers/mycalendar/utils/AssetUtils.kt
@@ -1,6 +1,7 @@
 package com.asivers.mycalendar.utils
 
 import android.content.Context
+import com.asivers.mycalendar.R
 import com.asivers.mycalendar.enums.Country
 import com.asivers.mycalendar.data.scheme.CountryHolidayScheme
 import com.asivers.mycalendar.data.scheme.TranslationScheme
@@ -21,4 +22,21 @@ fun getTranslationSchemeForExistingLocale(existingLocale: ExistingLocale, ctx: C
     val fileName = "translations" + File.separator + existingLocale.assetName + ".json"
     val json: String = ctx.assets.open(fileName).bufferedReader().use { it.readText() }
     return Json.decodeFromString<TranslationScheme>(json)
+}
+
+fun getYearIconId(year: Int): Int {
+    return when (year % 12) {
+        0 -> R.drawable.year_monkey
+        1 -> R.drawable.year_rooster
+        2 -> R.drawable.year_dog
+        3 -> R.drawable.year_pig
+        4 -> R.drawable.year_rat
+        5 -> R.drawable.year_ox
+        6 -> R.drawable.year_tiger
+        7 -> R.drawable.year_rabbit
+        8 -> R.drawable.year_dragon
+        9 -> R.drawable.year_snake
+        10 -> R.drawable.year_horse
+        else -> R.drawable.year_goat
+    }
 }
