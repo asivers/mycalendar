c7350b814de673fd8292969e3989db3630d07f57
asivers <sivers295@gmail.com>
2025-05-06 23:04:57 +0200

    upgraded agp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.9.2"
+    id("com.android.application") version "8.10.0"
     id("org.jetbrains.kotlin.android") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
