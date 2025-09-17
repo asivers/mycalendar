163987642944f8b97f5c7100b3ee094b5501eef9
asivers <sivers295@gmail.com>
2025-09-17 16:23:36 +0300

    updated app version for release to 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.asivers.mycalendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
