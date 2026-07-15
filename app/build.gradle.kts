f3de9ea6bce96fca11dcc7dcfcc3e11b3299feba
asivers <sivers295@gmail.com>
2026-07-15 21:03:09 +0300

    upgraded dependencies 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "9.2.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.0"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10"
     id("com.google.protobuf") version "0.10.0"
 }
 
